0b8ca2359561afc909967668eb4b552b093cee0a
Ayush <ayushreso1234@gmail.com>
2023-10-28 19:40:46 +0530

    ayush

---
+++ select.sql
@@ -0,0 +1,14 @@
+-- use to select any database
+
+use db;
+
+
+-- for viewing the id
+select id from emp_details;
+
+-- for viewing all the columns
+select * from details;
+
+
+
+
---
+++ where_clause.sql
@@ -0,0 +1,35 @@
+-- using the database db
+use db;
+
+
+-- creating an employe
+create table emp(
+emp_id int,
+name varchar(30),
+salary int,
+city varchar(20)
+);
+
+select * from emp;
+
+insert into emp
+(emp_id,name,salary,city)
+values (1,"aman",26000,"chennai"),(2,"suresh",29000,"chennai"),(3,"mahesh",28000,"mumbai"),(4,"monika",36000,"delhi"),
+(5,"jiesh",46000,"pune");
+
+select * from emp;
+
+-- using where clause conditioin that city is chennai
+select * 
+from emp
+where city="chennai";
+
+-- using where clause for salary more than 30000
+select *
+from emp
+where salary>30000;
+
+
+
+
+
